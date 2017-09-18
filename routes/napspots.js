9b1437e765f9f76ce895ece9434f02e746188427
Benjamin Liden <benjamin.c.liden@gmail.com>
2017-09-18 17:05:02 -0400

    User & Napspot linked. Added "Submitted by:" to show page.

--- routes/napspots.js
+++ routes/napspots.js
@@ -20,7 +20,11 @@ router.post("/", isLoggedIn, function(req, res){
 	var name = req.body.name;
 	var image = req.body.image;
   var description = req.body.description;
-	var newNapspot = {name: name, image: image, description: description};
+  var author = {
+    id: req.user._id,
+    username: req.user.username
+  };
+	var newNapspot = {name: name, image: image, description: description, author: author};
 	Napspot.create(newNapspot, function(err, newNapspot){
 		if(err){
 			console.log(err);
