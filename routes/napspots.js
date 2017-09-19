149cfb50d2c3d652010a2ef829495bbbcd3b5b3f
Benjamin Liden <benjamin.c.liden@gmail.com>
2017-09-19 11:23:54 -0400

    Protected napspot "edit", "update", "destroy". Edit & Delete buttons hidden if not logged in.

--- routes/napspots.js
+++ routes/napspots.js
@@ -55,19 +55,15 @@ router.get("/:id", function(req, res){
 
 // EDIT ROUTE
 
-router.get("/:id/edit", function(req, res){
-  Napspot.findById(req.params.id, function(err, foundNapSpot){
-    if(err){
-      res.redirect("/napspots");
-    } else {
-      res.render("napspots/edit", {napspot: foundNapSpot});
-    };
-  });
+router.get("/:id/edit", checkNapspotOwnership, function(req, res){
+    Napspot.findById(req.params.id, function(err, foundNapspot){
+          res.render("napspots/edit", {napspot: foundNapspot});
+    });
 });
 
 // UPDATE ROUTE
 
-router.put("/:id", function(req, res){
+router.put("/:id", checkNapspotOwnership, function(req, res){
   // find and update the correct napspot
   Napspot.findByIdAndUpdate(req.params.id, req.body.napspot, function(err, updatedNapspot){
     if(err){
@@ -81,7 +77,7 @@ router.put("/:id", function(req, res){
 
 // DELETE ROUTE
 
-router.delete("/:id", function(req, res){
+router.delete("/:id", checkNapspotOwnership, function(req, res){
   mongoose.findByIdAndRemove(req.params.id, function(err){
     if(err){
       res.redirect("/napspots");
@@ -99,5 +95,23 @@ function isLoggedIn(req, res, next){
   }
   res.redirect("/login");
 };
+
+function checkNapspotOwnership(req, res, next){
+  if(req.isAuthenticated()){
+    Napspot.findById(req.params.id, function(err, foundNapspot){
+      if(err){
+        res.redirect("back");
+      } else {
+        if(foundNapspot.author.id.equals(req.user._id)){
+          next();
+        } else {
+          res.redirect("back");
+        };
+      };
+    });
+  } else {
+    res.redirect("back");
+  };
+}
 
 module.exports = router;
